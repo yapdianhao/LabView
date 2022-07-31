a87e6c4d33b76901de517718eead43a920c16186
yapdianhao <dianhao.yap@2359media.com>
2022-08-01 00:41:14 +0800

    Add routing for pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import LoginPage from './components/LoginPage/LoginPage';
 import HomePage from './components/HomePage/HomePage';
 import UserManagementPage from './components/UserManagementPage/UserManagementPage';
+import AssetsPage from './components/AssetsPage/AssetsPage';
+import UtilizationsPage from './components/UtilizationsPage/UtilizationsPage';
+import RepairsPage from './components/RepairsPage';
+import PMCalsPage from './components/PMCalsPage/PMCalsPage';
+import ConsumablesPage from './components/ConsumablesPage/ConsumablesPage';
+import SchedulePage from './components/SchedulePage/SchedulePage';
+import ReportingPage from './components/ReportingPage/ReportingPage';
+import VendorsPage from './components/VendorsPage/VendorsPage';
+import AuditPage from './components/AuditPage/AuditPage';
 import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
@@ -12,16 +21,16 @@ function App() {
             <Routes>
                 <Route path='/' element={<HomePage />} />
                 <Route path='/login' element={<LoginPage />} />
-                <Route path='/assets' element={<HomePage />} />
-                <Route path='/utilizations' element={<HomePage />} />
-                <Route path='/repairs' element={<HomePage />} />
-                <Route path='/pmcals' element={<HomePage />} />
+                <Route path='/assets' element={<AssetsPage />} />
+                <Route path='/utilizations' element={<UtilizationsPage />} />
+                <Route path='/repairs' element={<RepairsPage />} />
+                <Route path='/pmcals' element={<PMCalsPage />} />
                 <Route path='/operators' element={<UserManagementPage />} />
-                <Route path='/consumables' element={<HomePage />} />
-                <Route path='/schedule' element={<HomePage />} />
-                <Route path='/reporting' element={<HomePage />} />
-                <Route path='/vendors' element={<HomePage />} />
-                <Route path='/audit' element={<HomePage />} />
+                <Route path='/consumables' element={<ConsumablesPage />} />
+                <Route path='/schedule' element={<SchedulePage />} />
+                <Route path='/reporting' element={<ReportingPage />} />
+                <Route path='/vendors' element={<VendorsPage />} />
+                <Route path='/audit' element={<AuditPage />} />
             </Routes>
         </Router>
       </>
---
+++ src/components/ConsumablesPage/ConsumablesPage.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const ConsumablesPage = () => {
+    return <div>Consumables Page</div>
+}
+
+export default ConsumablesPage;
