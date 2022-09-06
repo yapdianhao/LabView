db426270ef7befc5a1e4da167a8eae67a520787a
yapdianhao <dianhao.yap@2359media.com>
2022-09-07 01:56:27 +0800

    add semi design to user mgmt page

--- src/components/UserManagementPage/UserManagementPage.js
+++ src/components/UserManagementPage/UserManagementPage.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import NavBar from '../NavBar/NavBar';
+import { List, Checkbox, CheckboxGroup, Pagination } from '@douyinfe/semi-ui';
 import { USERS } from '../../mock/users';
 
+import styles from './UserManagementPage.module.css';
+
 const UserManagementPage = () => {
-    console.log(USERS);
     return (
-        <div>
+        <div className={styles.pageContainer}>
             <NavBar />
+            <div className={styles.contentWrapper}>
+                Test List
+            </div>
         </div>
     );
 }
